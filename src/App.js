8ccf70793589a53376f189cdcce5f943df5862c5
Deepanshu Dixit <deepanshudixit07@gmail.com>
2024-08-07 14:57:12 +0530

    rhtp base change

--- src/App.js
+++ src/App.js
@@ -261,7 +261,7 @@ function App() {
               ></textarea>
 
               <button
-                onClick={() => setShowInvoice(false)}
+                onClick={() => setShowInvoice(true)}
                 className="mt-5 bg-blue-500 text-white font-bold py-2 px-8 rounded shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 transition-all duration-300"
               >
                 Preview Invoice
